b945c651875abb4a682d22a281750bca3b661156
michael coleman <colemanmichael671@gmail.com>
2022-03-20 20:27:59 -0400

    Updates

---
+++ precache-manifest.318a2ce2934c8600f267696b12e8985e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a873064ca0a7e6391d30e871329e4e",
+    "url": "/mike-react-portfolio/index.html"
+  },
+  {
+    "revision": "f89b91b492c1e338eaa7",
+    "url": "/mike-react-portfolio/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "83ffa7834159861b3fd8",
+    "url": "/mike-react-portfolio/static/css/main.d25d04c0.chunk.css"
+  },
+  {
+    "revision": "f89b91b492c1e338eaa7",
+    "url": "/mike-react-portfolio/static/js/2.1ad833a8.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/mike-react-portfolio/static/js/2.1ad833a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08ac3a2c7d8e33f869c",
+    "url": "/mike-react-portfolio/static/js/3.32f9f42e.chunk.js"
+  },
+  {
+    "revision": "83ffa7834159861b3fd8",
+    "url": "/mike-react-portfolio/static/js/main.1e98cd11.chunk.js"
+  },
+  {
+    "revision": "536e65b4ebb0d8d1b876",
+    "url": "/mike-react-portfolio/static/js/runtime-main.5ee170af.js"
+  }
+]);
